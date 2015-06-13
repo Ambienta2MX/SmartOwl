835cd029559e8b4a614d134a3c30dbcb8da6feb2
egjimenezg <egjimenezg@gmail.com>
2015-06-13 14:37:10 -0500

    coding #2 adding class that will hold de api information

---
+++ src/main/groovy/mx/ipn/ambienta2mx/Api.groovy
@@ -0,0 +1,18 @@
+package com.ipn.ambienta2mx.smartOwl
+
+@Singleton
+class Api{
+
+  String apiUrl
+  String clientKey 
+
+  def getApiUrlRequest(){
+    "${apiUrl}/${clientKey}"
+  }  
+  
+  def setApiUrlAndClientKey(url,key){
+    apiUrl = url
+    clientKey = key
+  }
+
+}
