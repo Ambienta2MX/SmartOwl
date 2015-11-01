52e665a205551f04c209e98f433c35e99fce750e
egjimenezg <egjimenezg@gmail.com>
2015-10-31 22:20:37 -0600

    coding #5 fixing imports and adding configuration annotation

--- core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/config/DataAccessConfig.groovy
+++ core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/config/DataAccessConfig.groovy
@@ -3,10 +3,12 @@ package mx.ipn.ambienta2mx.smartOwl.config
 import org.springframework.context.annotation.Bean
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.StringRedisTemplate
-import org.springframework.data.redis.listener.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.listener.RedisMessageListenerContainer
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 class DataAccessConfiguration{
   
   @Bean
