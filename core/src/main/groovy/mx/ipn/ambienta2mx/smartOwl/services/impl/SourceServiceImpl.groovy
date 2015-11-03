b2a7de23417703884a653dd46ad774bdb1d822d4
egjimenezg <egjimenezg@gmail.com>
2015-11-02 21:25:08 -0600

    coding #6 finding all divs that represent a point in the map

--- core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/SourceServiceImpl.groovy
+++ core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/SourceServiceImpl.groovy
@@ -1,9 +1,11 @@
 package mx.ipn.ambienta2mx.smartOwl.services.impl
-import mx.ipn.ambienta2mx.smartOwl.Api
-import groovy.json.JsonSlurper
-import org.springframework.stereotype.Service
 
+import org.springframework.stereotype.Service
+import groovy.json.JsonSlurper
+import org.ccil.cowan.tagsoup.Parser
 import mx.ipn.ambienta2mx.smartOwl.services.SourceService
+import mx.ipn.ambienta2mx.smartOwl.Api
+import mx.ipn.ambienta2mx.smartOwl.enums.StateCode
 
 @Service
 class SourceServiceImpl implements SourceService{
@@ -18,13 +20,26 @@ class SourceServiceImpl implements SourceService{
     lines
   }
 
-  def getJSONFromUrlWithToken(url,token,coordinates){
+  def getJSONFromUrlWithToken(String url,String token,Map coordinates){
     def jsonSlurper = new JsonSlurper()
     def api = Api.instance
     api.setApiUrlAndClientKey(url,token)
     def connection = new URL("${api.getApiUrlRequest()}/${coordinates.latitude},${coordinates.longitude}")
     def jsonStructure = jsonSlurper.parseText(connection.text)
     jsonStructure.currently
+  }
+
+  def getFileUrlsForStations(StateCode stateCode){
+    def stationUrls = []
+    //TODO: Externalize conagua hostName
+    def hostName = "smn.cna.gob.mx"
+    def formedUrl = "emas/${stateCode.key}MP10T" 
+    def url = "http://${hostName}/${formedUrl}.html"
+
+    def slurper = new XmlSlurper(new Parser())
+    def htmlParse = slurper.parse(url)
+    stationUrls = htmlParse.'**'.find{ it.@id == 'mapa_solo' }.div.findAll{ it.@id.toString().startsWith("estacion") }*.@id 
+    stationUrls 
   }
 
 }
