480fd79bcdbe0ed65d77678842062d1a422cde0e
egjimenezg <egjimenezg@gmail.com>
2015-11-19 05:02:33 -0600

    coding #7 updating weather model

--- api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/domain/Weather.groovy
+++ api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/domain/Weather.groovy
@@ -6,12 +6,18 @@ class Weather{
   String description
   Double precipIntensity
   Double precipProbability
+  Double rainfallIntensity
+  Double rainfallProbability
   Double temperature  
   Double apparentTemperature
+  Double dewPoint
   Double humidity
   Double windSpeed
+  Double windDirection
   Double windBearing  
   Double visibility
   Double cloudCover
   Double pressure
+  List<String> provider
+  Date dateCreated
 }
