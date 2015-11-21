e1548ce24be3f7f92635f578c788bd08a7c8a76a
egjimenezg <egjimenezg@gmail.com>
2015-11-20 21:10:33 -0600

    coding #6 renaming method in the interface

--- api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/SourceService.groovy
+++ api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/SourceService.groovy
@@ -4,6 +4,6 @@ import mx.ipn.ambienta2mx.smartOwl.enums.StateCode
 interface SourceService{
 
   def getJSONFromUrlWithToken(String url,String token,Map coordinates)
-  def getFileUrlsForStations(StateCode stateCode)
+  def getFileUrlsForStation(StateCode stateCode)
 
 }
