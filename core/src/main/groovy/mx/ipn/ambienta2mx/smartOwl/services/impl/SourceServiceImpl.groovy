b247c344c8ea1d07f22277a3b62eea31787547d3
egjimenezg <egjimenezg@gmail.com>
2015-11-22 14:45:25 -0600

    coding #6 getting coordinates from file

--- core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/SourceServiceImpl.groovy
+++ core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/SourceServiceImpl.groovy
@@ -39,9 +39,17 @@ class SourceServiceImpl implements SourceService{
     }
   }
 
-  def convertCoordinatesToDecimal(def coordinates){
-    def values = coordinates.collect{ new Float(it) }
-    new BigDecimal(values[0]+values[1]/60+values[2]/3600).setScale(4,BigDecimal.ROUND_HALF_UP)
+  def convertCoordinatesToDecimal(def degressLatitude,def degressLongitude){
+    def jsonSlurper = new JsonSlurper()
+    def latitudeValues = degressLatitude.collect{ new Float(it) }
+    def longitudeValues = degressLongitude.collect{ new Float(it) }
+    def decimalLatitude = new BigDecimal(latitudeValues[0]+latitudeValues[1]/60+latitudeValues[2]/3600).setScale(4,BigDecimal.ROUND_HALF_UP)
+    def decimalLongitude = new BigDecimal(longitudeValues[0]+longitudeValues[1]/60+longitudeValues[2]/3600).setScale(4,BigDecimal.ROUND_HALF_UP)
+    def connection = new URL("http://mapserver.inegi.org.mx/traninv/servicios/geo/itrf92/${decimalLongitude}/${decimalLatitude}")
+    def jsonStructure = jsonSlurper.parseText(connection.text)[0].itrf92
+
+    [latitude:jsonStructure.y,
+     longitude:jsonStructure.x]
   } 
   
   def getFileUrlsOfCountry(){
@@ -52,6 +60,14 @@ class SourceServiceImpl implements SourceService{
     }
 
     countryFileUrls
+  }
+
+  def getUrlCoordinates(String fileUrl){
+    def url = "http://smn.cna.gob.mx/emas/txt/BC05_10M.TXT"  
+    def writer = url.toURL().filterLine{ it ==~ /.*[0-9].*\".*/ }
+    def latitude_longitude = writer.toString().replaceAll(/[^\d]/," ")
+    latitude_longitude = (latitude_longitude =~ /([0-9]+\s){3}/)
+    convertCoordinatesToDecimal(latitude_longitude[1][0].tokenize(),latitude_longitude[0][0].tokenize())
   }
 
 }
