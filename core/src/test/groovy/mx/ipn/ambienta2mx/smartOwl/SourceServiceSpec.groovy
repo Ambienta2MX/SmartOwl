5015c597ba4f146bf4be8f3dce684717a976fa99
egjimenezg <egjimenezg@gmail.com>
2015-11-27 18:31:16 -0600

    coding #3 removing unnecessary tests

--- core/src/test/groovy/mx/ipn/ambienta2mx/smartOwl/SourceServiceSpec.groovy
+++ core/src/test/groovy/mx/ipn/ambienta2mx/smartOwl/SourceServiceSpec.groovy
@@ -39,44 +39,4 @@ class SourceServiceSpec extends Specification{
         23.543  | -103.025
   }
 
-  Should "get the urls of each station"(){ 
-    given:"the state code"
-      def stateCode = StateCode.DF
-    when:
-      def urls = service.getFileUrlsForStation(stateCode)
-    then:
-      urls.size() == 63  
-      urls.first().startsWith("http://smn.cna.gob.mx")
-  }
-  
-  Should "get the urls of the country"(){
-    given:
-      StateCode.metaClass.static.values = {[StateCode.BC,StateCode.DF]}
-    when:
-      def countryUrls = service.getFileUrlsOfCountry()
-    then:
-      countryUrls.first().latitude.class.simpleName == "BigDecimal"
-      countryUrls.first().url.class.simpleName == "String"
-  }
-
-  Should "get the decimal coordinates from degrees to decimal"(){
-    given:"the longitude with minutes and seconds"
-      def latitude = ["28","53","47"]
-      def longitude = ["113","33","37"] 
-    when:
-      def decimalCoordinates = service.convertCoordinatesToDecimal(latitude,longitude)
-    then:
-      decimalCoordinates.latitude == 28.8964
-      decimalCoordinates.longitude == -113.5603
-  }
-  
-  Should "get the latitude and longitude of the file"(){
-    given:"the url"
-      def fileUrl = "http://smn.cna.gob.mx/emas/txt/BC05_10M.TXT"
-    when:
-      def coordinates= service.getUrlCoordinates(fileUrl)
-    then:
-      coordinates.latitude == 28.8964
-      coordinates.longitude == -113.5603
-  }
 }
