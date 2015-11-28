f989b1bbb1dde2f300468bad29337ab28cf8a6c0
egjimenezg <egjimenezg@gmail.com>
2015-11-27 22:35:37 -0600

    coding #3 updating service interface

--- api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/SourceService.groovy
+++ api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/SourceService.groovy
@@ -4,6 +4,5 @@ import mx.ipn.ambienta2mx.smartOwl.enums.StateCode
 interface SourceService{
 
   def getJSONFromUrlWithToken(String url,String token,Map coordinates)
-  def getFileUrlsForStation(StateCode stateCode)
 
 }
