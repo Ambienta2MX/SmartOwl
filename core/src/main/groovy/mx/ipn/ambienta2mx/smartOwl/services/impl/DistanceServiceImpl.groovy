e69a9635006e05a041430b63def36a154d9fc98e
egjimenezg <egjimenezg@gmail.com>
2015-11-28 23:50:57 -0600

    coding #3 adding class with the methods implementation

---
+++ core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/DistanceServiceImpl.groovy
@@ -0,0 +1,12 @@
+package mx.ipn.ambienta2mx.smartOwl.services.impl
+
+import org.springframework.stereotype.Service
+import mx.ipn.ambienta2mx.smartOwl.services.DistanceService
+
+@Service
+class DistanceServiceImpl implements DistanceService{
+
+  def levenshteinDistance(String stringA,String stringB){
+
+  } 
+}
