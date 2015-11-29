4bac77a2499340a357be5a387b5ef98bf69768ed
egjimenezg <egjimenezg@gmail.com>
2015-11-29 13:03:57 -0600

    coding #3 adding algorithm implementation

--- core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/DistanceServiceImpl.groovy
+++ core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/DistanceServiceImpl.groovy
@@ -6,11 +6,44 @@ import mx.ipn.ambienta2mx.smartOwl.services.DistanceService
 @Service
 class DistanceServiceImpl implements DistanceService{
 
-  def levenshteinDistance(String stringA,String stringB){
+  def getLevenshteinDistance(String stringA,String stringB){
+    def m = stringA.size()
+    def n = stringB.size()
+    int[][] matrix = new int[m+1][n+1]
+    fillMatrixWithZero(matrix)
 
+    (1..m).each{ i ->
+      matrix[i][0] = i
+    }
+
+    (1..n).each{ j ->
+      matrix[0][j] = j
+    }
+
+    (1..n).each{ j ->
+      (1..m).each{ i ->
+        if(stringA[i-1] == stringB[j-1])
+          matrix[i][j] = matrix[i-1][j-1]
+        else
+          matrix[i][j] = minimum(matrix[i-1][j]+1,
+                                 matrix[i][j-1]+1,
+                                 matrix[i-1][j-1]+1)
+      }
+    }
+
+    matrix[m][n]
   } 
 
   def minimum(Long a,Long b,Long c){
     Math.min(Math.min(a,b),c)
   }
+
+  private void fillMatrixWithZero(def matrix){
+    matrix.size().times{ i ->
+      matrix[i].size().times{ j ->
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
 }
