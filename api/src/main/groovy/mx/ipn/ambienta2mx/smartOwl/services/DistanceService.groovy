17b8d4e0290c65a2a464ddd8b08537395a1a2030
egjimenezg <egjimenezg@gmail.com>
2015-11-28 23:44:43 -0600

    coding #3 adding method definitio and new class

---
+++ api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/DistanceService.groovy
@@ -0,0 +1,5 @@
+package mx.ipn.ambienta2mx.smartOwl.services
+
+interface DistanceService{
+  def levenshteinDistance(String stringA,String stringB)
+}
--- api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/ParseDataService.groovy
+++ api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/ParseDataService.groovy
@@ -1,6 +1,7 @@
 package mx.ipn.ambienta2mx.smartOwl.services
 
 interface ParseDataService{
+  def getWeatherModelFromFile(File file)
   def getWeatherModelFromJSON(latitude,longitude);
   def getPollutionModelFromJSON(latitude,longitude);
 }
