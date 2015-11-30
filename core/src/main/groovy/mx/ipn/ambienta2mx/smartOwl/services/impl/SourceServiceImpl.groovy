acf80a426cc3429897722f0a5fb711a8be43930f
egjimenezg <egjimenezg@gmail.com>
2015-11-29 18:05:44 -0600

    coding #9 adding method implementation to get the tables with pollution info

--- core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/SourceServiceImpl.groovy
+++ core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/SourceServiceImpl.groovy
@@ -2,7 +2,6 @@ package mx.ipn.ambienta2mx.smartOwl.services.impl
 
 import org.springframework.stereotype.Service
 import groovy.json.JsonSlurper
-import org.ccil.cowan.tagsoup.Parser
 import mx.ipn.ambienta2mx.smartOwl.services.SourceService
 import mx.ipn.ambienta2mx.smartOwl.Api
 import mx.ipn.ambienta2mx.smartOwl.enums.StateCode
@@ -18,5 +17,16 @@ class SourceServiceImpl implements SourceService{
     def jsonStructure = jsonSlurper.parseText(connection.text)
     jsonStructure.currently
   }
+
+  def getTablesWithPollutionData(String imageCode){
+    //TODO:Externalize URL
+    def url = "http://sg1.aqicn.org/aqicn/cache/webwgt/${imageCode}/widget.v1.js" 
+    def text = url.toURL().readLines().first()
+    def jsonSlurper = new JsonSlurper()
+    text = text.replaceAll(/aqicnWidgetLoaderCallback\(|\);try.*\*\/|src='data:image\/png;base64,[a-zA-Z0-9+\/]+={0,2}'|nowrap[=true]?|true/,"")
+    def json = jsonSlurper.parseText(text)
+    def tables = (json.xxl =~ /<table.*>.*<\/table>/)[0]
+    tables 
+  } 
 
 }
