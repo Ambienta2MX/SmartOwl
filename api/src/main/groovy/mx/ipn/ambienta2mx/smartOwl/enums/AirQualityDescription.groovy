3579db13da1480c180e5ea286b5ed13bee3eab56
egjimenezg <egjimenezg@gmail.com>
2015-11-30 02:06:10 -0600

    coding #11 adding new enum for the air quality

---
+++ api/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/enums/AirQualityDescription.groovy
@@ -0,0 +1,16 @@
+package mx.ipn.ambienta2mx.smartOwl.enums
+
+enum AirQualityDescription{
+  MODERATE("Moderado"),
+  GOOD("Bueno"),
+  NO_DATA("Sin datos"),
+  UNHEALTHY("Insalubre"),
+  VERY_UNHEALTHY("Muy insalubre"),
+  HAZARDOUS("Arriesgado")
+
+  final String value
+  AirQualityDescription(String value){ this.value = value }
+
+  String toString(){ value }
+  String getKey(){ name() }
+}
