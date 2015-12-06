3fb1198e45e498522c5a080410efa1c618af40cb
egjimenezg <egjimenezg@gmail.com>
2015-12-05 23:55:10 -0600

    coding #11 sending data to HardAnt for save the data

--- core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/PollutionServiceImpl.groovy
+++ core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/PollutionServiceImpl.groovy
@@ -4,6 +4,7 @@ import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import wslite.rest.*
 
 @Service
 class PollutionServiceImpl{
@@ -16,10 +17,16 @@ class PollutionServiceImpl{
 
   def findPollutionModel(String latitude,String longitude){
     // conaguaStation[url], weatherUndergoundStation[id]
-    def url = new URL("http://localhost/api/fasteagle/pollutionStation?latitude=$latitude&longitude=$longitude")
+    def url = new URL("http://127.0.0.1:7777/pollutionStation?latitude=$latitude&longitude=$longitude")
     def imageCode = new JsonSlurper().parse(url).img
     def html = sourceService.getTablesWithPollutionData(imageCode)
-    parseDataService.getPollutionModelFromHtml(html)
-    //TODO post to localhost/api/hardant/pollution
+    def pollutionModel = parseDataService.getPollutionModelFromHtml(html)
+    //TODO: Externalize HardAntUrl
+    def client = new RESTClient("http://127.0.0.1:7001")
+    def response = client.post(path:"/pollution",query:[sulphurDiode:pollutionModel.sulphurDiode,
+                                                        latitude:latitude,longitude:longitude])
+    
+    pollutionModel
   }
+
 }
