8853f258ed7de7e1465d79500c5fa33ae5ddea67
egjimenezg <egjimenezg@gmail.com>
2015-12-14 19:42:14 -0600

    coding #12 updating weather controller to show de info

--- core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/controllers/WeatherController.groovy
+++ core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/controllers/WeatherController.groovy
@@ -8,19 +8,18 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import mx.ipn.ambienta2mx.smartOwl.services.impl.ParseDataServiceImpl
+import mx.ipn.ambienta2mx.smartOwl.services.impl.WeatherServiceImpl
 
 @RestController
 @RequestMapping(value="/weather")
 class WeatherController{
   
   @Autowired
-  ParseDataServiceImpl parseDataService
+  WeatherServiceImpl weatherServiceImpl 
 
   @RequestMapping(method= RequestMethod.GET)
   ResponseEntity<Map> show(@RequestParam(value="latitude")String latitude,@RequestParam(value="longitude")String longitude){
-  
-    def model = parseDataService.getWeatherModelFromJSON(latitude,longitude)
+    def model = weatherServiceImpl.findWeatherModel(latitude,longitude)
     new ResponseEntity<Map>(model,HttpStatus.OK)
   }
 }
