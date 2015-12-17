939c73a0b683b29e16da47ae076e0cedaa73afc8
egjimenezg <egjimenezg@gmail.com>
2015-12-17 12:58:22 -0600

    coding #15 updating methods implementation

--- core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/PollutionServiceImpl.groovy
+++ core/src/main/groovy/mx/ipn/ambienta2mx/smartOwl/services/impl/PollutionServiceImpl.groovy
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import wslite.rest.*
 import mx.ipn.ambienta2mx.smartOwl.services.PollutionService
+import mx.ipn.ambienta2mx.smartOwl.domain.Pollution
 
 @Service
 class PollutionServiceImpl implements PollutionService{
@@ -23,10 +24,17 @@ class PollutionServiceImpl implements PollutionService{
   SourceServiceImpl sourceService
 
   def findPollutionModel(String latitude,String longitude){
-    def url = new URL("${fastEagleUrl}/pollutionStation?latitude=$latitude&longitude=$longitude")
-    def imageCode = new JsonSlurper().parse(url).img
-    def html = sourceService.getTablesWithPollutionData(imageCode)
-    def pollutionModel = parseDataService.getPollutionModelFromHtml(html)
+    def pollution = new Pollution()
+    pollution.dateCreated = new Date() 
+    pollution = findPollutionModelFromAirQuality(latitude,longitude,pollution)
+    pollution = findPollutionModelFromForecast(latitude,longitude,pollution)
+    def fields = pollution.class.declaredFields.findAll{ !it.synthetic }*.name
+    def pollutionModel = [:]
+
+    fields.each{ field ->
+      pollutionModel[field] = pollution[field]
+    }
+
     pollutionModel.latitude = latitude
     pollutionModel.longitude = longitude
     def client = new RESTClient("${hardAntUrl}")
@@ -38,4 +46,21 @@ class PollutionServiceImpl implements PollutionService{
     new JsonSlurper().parseText(response.contentAsString) 
   }
 
+  def findPollutionModelFromAirQuality(String latitude,String longitude,Pollution pollution){
+    def url = new URL("${fastEagleUrl}/pollutionStation?latitude=$latitude&longitude=$longitude")
+    def imageCode = new JsonSlurper().parse(url).img
+    def html = sourceService.getTablesWithPollutionData(imageCode)
+    def fields = pollution.class.declaredFields.findAll{ !it.synthetic }*.name
+    def pollutionModel = parseDataService.getPollutionModelFromHtml(html)
+
+    if(pollutionModel)
+      pollution.provider << "Air Quality" 
+
+    pollution
+  }
+
+  def findPollutionModelFromForecast(String latitude,String longitude,Pollution pollution){
+    parseDataService.getPollutionModelFromJSON(latitude,longitude,pollution)
+  }
+ 
 }
